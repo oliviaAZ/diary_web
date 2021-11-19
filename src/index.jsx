9c91dc4abf8c4b2f47605fe0955c590dd9fdf7a7
oliviaAZ <oliviazxj@sina.com>
2021-11-19 16:25:14 +0800

    update

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import RouterMap from "./Router";
-import "@babel/polyfill";
+// import "@babel/polyfill";
 import "antd-mobile/dist/antd-mobile.css";
 
 ReactDOM.render(<RouterMap />, document.getElementById("app"));
